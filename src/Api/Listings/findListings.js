771bd1f4f3ff4271f7b56e5cf7b665bade83c812
frustrated018 <niloyd018@gmail.com>
2024-03-29 13:34:37 +0600

    Updated findListings
    Insted of using .find method i'm currently using .aggregate to get all the necessary documents and also sending some metadata about the totalDocuments the pagenumber and the total pages

--- src/Api/Listings/findListings.js
+++ src/Api/Listings/findListings.js
@@ -1,16 +1,66 @@
 const Listing = require("../../Models/Listing");
 
-//TODO: Get the select fields for the query or body of the request and give data based on that
-//TODO: Paginate this data to make this more efficient
-
 const findListings = async (req, res) => {
-  try {
-    const listings = await Listing.find().select("name summary");
+  let { page, limit } = req.query;
 
-    res.send(listings);
+  page = parseInt(page) || 1;
+  limit = parseInt(limit) || 10;
+
+  try {
+    const result = await Listing.aggregate([
+      {
+        $facet: {
+          metaData: [
+            {
+              $count: "totalDocuments",
+            },
+            {
+              $addFields: {
+                pageNumber: page,
+                totalPages: { $ceil: { $divide: ["$totalDocuments", limit] } },
+              },
+            },
+          ],
+          data: [
+            {
+              $project: {
+                name: 1,
+                medium_url: 1,
+                review_scores_rating: 1,
+                room_type: 1,
+                price: 1,
+              },
+            },
+            {
+              $skip: (page - 1) * limit,
+            },
+            {
+              $limit: limit,
+            },
+          ],
+        },
+      },
+    ]);
+
+    // if else condition to handle the result array being empty
+    if (result.length > 0) {
+      const listings = result[0];
+      listings.metaData = {
+        ...listings.metaData[0],
+        count: listings.data.length,
+      };
+      res.send(listings);
+    } else {
+      res.status(404).send({ message: "No listings found" });
+    }
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
 };
 
 module.exports = findListings;
+
+//.find()
+// .select("name medium_url review_scores_rating room_type price")
+// .skip((page - 1) * limit)
+// .limit(limit);
