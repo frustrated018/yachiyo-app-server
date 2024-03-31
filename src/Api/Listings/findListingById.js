a5281117c55c6fb2d2192f2a545c2f71f587fea6
frustrated018 <niloyd018@gmail.com>
2024-03-31 20:34:13 +0600

    Added select to filter and send only necessary data

--- src/Api/Listings/findListingById.js
+++ src/Api/Listings/findListingById.js
@@ -3,11 +3,12 @@ const Listing = require("../../Models/Listing");
 const findListingById = async (req, res) => {
   const { id } = req.query;
 
-  //TODO: run a select method to select only the necessary fields
   //TODO:Also see if you can find way to aggrigate this method
 
   try {
-    const result = await Listing.findById(id);
+    const result = await Listing.findById(id).select(
+      "xl_picture_url name room_type smart_location bedrooms beds bathrooms review_scores_rating number_of_reviews host_name host_picture_url host_since description price"
+    );
     res.send(result);
   } catch (error) {
     res.status(500).send({ message: "Error Fetching Data", error });
